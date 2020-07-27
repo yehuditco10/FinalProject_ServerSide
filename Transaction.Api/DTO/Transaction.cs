b1457d9ddf2f641e9b40331748b26b8caeae8337
batya-hartman <bt3232675@gmail.com>
2020-07-27 16:36:11 +0300

    annotation

--- Transaction.Api/DTO/Transaction.cs
+++ Transaction.Api/DTO/Transaction.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Transaction.Api.DTO
 {
     public class Transaction
     {
+        [Required]
         public Guid FromAccountId { get; set; }
+        [Required]
         public Guid ToAccountId { get; set; }
+        [Required]
         public int Amount { get; set; }
     }
 }
